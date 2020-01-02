d336d134c0cb9da622ec52fe589eb20ae4d0e983
Mr-Wii <erubakugeki@gmail.com>
2020-01-02 11:10:09 +0100

    feat: contact form added

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,41 +1,38 @@
 import React from 'react'
-import axios from 'axios'
 import './style.css'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 class Contact extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
+    this.state = { name: '', email: '', message: '' }
   }
 
-  handleSubmit(e) {
-    e.preventDefault()
-    axios({
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch('/', {
       method: 'POST',
-      url: 'http://localhost:3002/send',
-      data: this.state
-    }).then(response => {
-      if (response.data.status === 'success') {
-        alert('Message Sent.')
-        this.resetForm()
-      } else if (response.data.status === 'fail') {
-        alert('Message failed to send.')
-      }
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state })
     })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error))
+
+    e.preventDefault()
   }
 
-  resetForm() {
-    this.setState({ name: '', email: '', message: '' })
-  }
-
+  handleChange = e => this.setState({ [e.target.name]: e.target.value })
   render() {
+    const { name, email, message } = this.state
     return (
       <React.Fragment>
         <CssBaseline />
@@ -52,27 +49,27 @@ class Contact extends React.Component {
               <form
                 className="cf"
                 id="contact-form"
-                onSubmit={this.handleSubmit.bind(this)}
-                method="POST"
+                name="contact"
+                netlify
+                onSubmit={this.handleSubmit}
               >
+                <input type="hidden" name="form-name" value="contact" />
                 <div className="half left cf">
                   <input
                     type="text"
                     id="input-name"
                     placeholder="Full Name"
                     name="name"
-                    // id="name"
-                    value={this.state.name}
-                    onChange={this.onNameChange.bind(this)}
+                    value={name}
+                    onChange={this.handleChange}
                   />
                   <input
                     type="email"
                     id="input-email"
                     placeholder="Email address"
                     name="email"
-                    // id="email"
-                    value={this.state.email}
-                    onChange={this.onEmailChange.bind(this)}
+                    value={email}
+                    onChange={this.handleChange}
                   />
                   <input type="text" id="input-subject" placeholder="Subject" />
                 </div>
@@ -82,82 +79,18 @@ class Contact extends React.Component {
                     type="text"
                     id="input-message"
                     placeholder="Message"
+                    value={message}
+                    onChange={this.handleChange}
                     // id="message"
-                    value={this.state.message}
-                    onChange={this.onMessageChange.bind(this)}
                   ></textarea>
                 </div>
                 <input type="submit" value="Submit" id="input-submit" />
               </form>
-              {/* <form
-                id="contact-form"
-                onSubmit={this.handleSubmit.bind(this)}
-                method="POST"
-                className="contact100-form validate-form"
-              >
-                <span className="contact100-form-title">Contact Us</span>
-                <div
-                  className="wrap-input100 validate-input"
-                  data-validate="Please enter your name"
-                >
-                  <input />
-                  <span className="focus-input100"></span>
-                </div>
-                <div
-                  className="wrap-input100 validate-input"
-                  data-validate="Please enter email: e@a.x"
-                >
-                  <input
-                    type="email"
-                    className="input100"
-                    name="email"
-                    placeholder="Email"
-                    id="email"
-                    aria-describedby="emailHelp"
-                    value={this.state.email}
-                    onChange={this.onEmailChange.bind(this)}
-                  />
-                  <span className="focus-input100"></span>
-                </div>
-                <div
-                  className="wrap-input100 validate-input"
-                  data-validate="Please enter your message"
-                >
-                  <textarea
-                    className="input100"
-                    name="message"
-                    placeholder="Your Message"
-                    rows="5"
-                    id="message"
-                    value={this.state.message}
-                    onChange={this.onMessageChange.bind(this)}
-                  />
-                  <span className="focus-input100"></span>
-                </div>
-                <div className="container-contact100-form-btn">
-                  <button type="submit" className="contact100-form-btn">
-                    Submit
-                  </button>
-                </div>
-              </form> */}
             </div>
           </Typography>
         </Container>
       </React.Fragment>
     )
   }
-
-  onNameChange(event) {
-    this.setState({ name: event.target.value })
-  }
-
-  onEmailChange(event) {
-    this.setState({ email: event.target.value })
-  }
-
-  onMessageChange(event) {
-    this.setState({ message: event.target.value })
-  }
 }
-
 export default Contact
